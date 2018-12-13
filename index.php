f955fc4e08c9ad54ee38e84dc67da67051d995bf
kholekeyur <37572445+kholekeyur@users.noreply.github.com>
2018-12-13 15:50:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
-echo "<h1>HELLO KEYUR jj</h1>";
+ 
+         $db_connection = pg_connect ("  host=ec2-46-51-184-66.eu-west-1.compute.amazonaws.com 
+                                        dbname=dbns3a2p2i30bj 
+                                        user=zthovxzrbvlazp 
+                                        password=2877bd9ce71d8d9b8c0cb27ed2a41c31863f6468a28183867e9c682bed0fc73b"  );
+
+
+      var_dump($db_connection);
+
 
 ?>
